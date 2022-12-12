71a58cf416604a6416508f5a5ecf2695df35e54d
vjp-jorgeCC <114688141+vjp-jorgeCC@users.noreply.github.com>
2022-12-13 00:31:26 +0100

    por culpa de sara

--- Tema 4/Ejercicio 09 T4/src/ejercicio/pkg09/t4/Ejercicio09T4.java
+++ Tema 4/Ejercicio 09 T4/src/ejercicio/pkg09/t4/Ejercicio09T4.java
@@ -8,36 +8,14 @@ package ejercicio.pkg09.t4;
 import java.util.Scanner;
 
 public class Ejercicio09T4 {
-    public static int numero(){
-        int dato;
-        Scanner entrada=new Scanner (System.in);
-        System.out.println("Introduce un numero: ");
-        dato=entrada.nextInt();
-        return dato;
-    }
-    public static void menor (int numero1, int numero2, int numero3){
-        int numeroMenor=0;
-        if ((numero1<numero2)&&(numero1<numero3)) {
-            numeroMenor=numero1;
-        }
-        else{
-            if ((numero2<numero1)&&(numero2<numero3)) {
-                numeroMenor=numero2;
-            }
-            else{
-                numeroMenor=numero3;
-            }
-        }
-        System.out.println("El numero menor de "+numero1+" - "+numero2+" - "+numero3+" es: "+numeroMenor);
-    }
     public static void main(String[] args) {
        int numero1;
        int numero2;
        int numero3;
-       numero1=numero();
-       numero2=numero();
-       numero3=numero();
-       menor(numero1,numero2,numero3);
+       numero1=Menor.numero();
+       numero2=Menor.numero();
+       numero3=Menor.numero();
+       Menor.menor(numero1,numero2,numero3);
     }
     
 }
