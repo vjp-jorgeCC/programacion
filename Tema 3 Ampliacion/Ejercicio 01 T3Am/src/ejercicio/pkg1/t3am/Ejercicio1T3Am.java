44d1433f306e6dd8813279ec07ec2e01be366315
vjp-jorgeCC <114688141+vjp-jorgeCC@users.noreply.github.com>
2022-12-12 23:56:37 +0100

    Add files via upload

---
+++ Tema 3 Ampliacion/Ejercicio 01 T3Am/src/ejercicio/pkg1/t3am/Ejercicio1T3Am.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio.pkg1.t3am;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author jorge
+ */
+public class Ejercicio1T3Am {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner (System.in);
+        int numero;        
+        do {
+            System.out.println("Introduce un numero: ");
+            numero=entrada.nextInt();
+            for (int i = 0; i < numero; i++) {
+            System.out.println("hola");            
+            }
+        } while (numero<0);
+    }
+    
+}
